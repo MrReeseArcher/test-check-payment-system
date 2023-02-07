283f65916d9e1d6dc589ed65847f9aed44139c0c
ta_aleks <Oneofthemany1>
2023-02-07 10:15:45 +0300

    master

--- application/routes/api.php
+++ application/routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('files')->group(function () {
+Route::prefix('files')->middleware(['auth:sanctum'])->group(function () {
     Route::post('check-payment-system', [FileHandlerController::class, 'checkPaymentSystem']);
 });
